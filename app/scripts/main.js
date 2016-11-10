8cbdf7e88594950f3d7a3d7cb16c8943662a87bd
Gromek Marcin <mgromek@efigence.com>
2016-11-11 00:16:08 +0100

    move js to another location

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -72,5 +72,51 @@
     });
   }
 
-  // Your custom JavaScript goes here
+    $(document).ready(function() {
+
+      var app = {
+
+        init: function() {
+          this.scrollToTop();
+          this.backToTop();
+          this.menuLinkSpy();
+        },
+
+        scrollToTop: function() {
+          $(window).scroll(function() {
+            if ($(this).scrollTop() > 800) {
+                $('#top').fadeIn();
+            } else {
+                $('#top').fadeOut();
+            }
+          });
+        },
+
+        backToTop: function() {
+          var $btn = $('#top');
+
+          $btn.on('click', function() {
+            $('html, body').animate({scrollTop:0}, 800);
+            return false;
+          });
+        },
+
+        menuLinkSpy: function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                  $('html,body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000);
+                return false;
+              }
+            }
+          });
+        }
+
+      };
+      app.init();
+  });
 })();
